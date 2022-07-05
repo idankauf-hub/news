425134bebc7dacee60bf1c5ed3bafc0ef69e9172
Idan <idankauf@gmail.com>
2022-07-05 17:31:39 +0300

    drop down added

---
+++ src/Comps/dropdown/DropDown.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './dropdown.css'
+const DropDown = () => {
+  return (
+    <select className='dropDown'>
+        <option>asdsa</option>
+    </select>
+  )
+}
+
+export default DropDown
--- src/Comps/search/Search.tsx
+++ src/Comps/search/Search.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './search.css'
 import { ReactComponent as SearchIcon } from '../../Assets/search.svg';
+import DropDown from '../dropdown/DropDown';
 
 const Search = () => {
   return (
@@ -8,6 +9,7 @@ const Search = () => {
         <button className='input_submit' type='submit'><SearchIcon/></button>
         <input type='input' className='input_box' placeholder='Search'></input>
         <div className="vl"></div>
+        <DropDown/>
     </form>
     )
 }
