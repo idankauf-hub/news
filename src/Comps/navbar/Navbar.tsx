a94dcf8dc749d55e4d50725a47853df9635ca1aa
Idan <idankauf@gmail.com>
2022-07-06 14:14:50 +0300

    setup

---
+++ src/Comps/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./navbar.css"
+import { ReactComponent as Logo } from '../../Assets/Dispatcher Logo.svg';
+// import Search from '../search/Search';
+
+export const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <Logo/>
+      </div>
+        {/* <Search/> */}
+    </div>
+  )
+}
---
+++ src/Comps/navbar/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const NavBarContainer = styled.div`
+height:74px;
+width: 100%;
+background-color:#262146 ;
+display: flex;
+flex-direction: row;
+align-items: center;
+gap: 250px;
+border: 1px solid #262146;
+box-shadow: 0px 32px 64px rgba(0, 0, 0, 0.05);
+border-radius: 0px;
+`;
