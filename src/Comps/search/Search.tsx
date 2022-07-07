f1d5efde9e212b50bdfd82b093dbfe90191ced76
Idan <idankauf@gmail.com>
2022-07-07 15:14:41 +0300

    Debounce added

--- src/Comps/search/Search.tsx
+++ src/Comps/search/Search.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, useState ,useEffect} from 'react';
 import { SearchIcon } from "../../Icons";
 import {
   SearchFormContainer,
@@ -6,15 +6,28 @@ import {
   Input,
   VerticalLine,
 } from "./style";
+import { useDebounce } from 'usehooks-ts'
+
 // import DropDown from '../dropdown/DropDown';
 
 const Search = () => {
+  const [input, setInput] = useState<string>("");
+  const debouncedValue = useDebounce<string>(input, 500)
+
+  const handleChange=(event: ChangeEvent<HTMLInputElement>)=>{
+    setInput(event.currentTarget.value)
+    console.log(input)
+  };
+  
+  useEffect(() => {
+   console.log(input)
+  }, [debouncedValue])
   return (
     <SearchFormContainer>
       <SearchButton>
         <SearchIcon />
       </SearchButton>
-      <Input></Input>
+      <Input onChange={handleChange}></Input>
       <VerticalLine></VerticalLine>
       {/* <DropDown/> */}
     </SearchFormContainer>
