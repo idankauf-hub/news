77f4988db497d6ccabf042fdfbacfe19983d9a08
Idan <idankauf@gmail.com>
2022-07-07 13:59:21 +0300

    drop down without icon

---
+++ src/Comps/DropDown/DropDown.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Select} from './style'
+import { ForwardIcon } from "../../Icons/index";
+
+const DropDown = () => {
+  return (
+    <Select>
+        <option>fed</option>
+    </Select>
+  )
+}
+
+export default DropDown
---
+++ src/Comps/DropDown/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import { COLORS } from "../../globalStyle";
+import { ForwardIcon } from "../../Icons/index";
+
+export const Select = styled.select`
+position: absolute;
+display:flex;
+flex-direction:row;
+border:none;
+padding: 0 0 0 16px;
+left:264px;
+top:5px;
+height: 40px;
+width:138.93px;
+appearance: none;
+//   background-image: url(});
+  background-repeat: no-repeat;
+  background-position: 100% 0%;
+`;
