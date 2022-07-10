f82f934d59c482f67621a10ecc8edeeb712dbb0c
Idan <idankauf@gmail.com>
2022-07-10 17:40:00 +0300

    svg added to dropdown

--- src/Comps/DropDown/style.ts
+++ src/Comps/DropDown/style.ts
@@ -12,8 +12,11 @@ left:264px;
 top:5px;
 height: 40px;
 width:138.93px;
-appearance: none;
-//   background-image: url(});
-  background-repeat: no-repeat;
-  background-position: 100% 0%;
+border: none;
+background: blue url("data:image/svg+xml;utf8,<svg width="18" height="10" viewBox="0 0 18 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M16.3334 1.33334L9.00004 8.66668L1.66671 1.33334" stroke="#5A5A89" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>") no-repeat;
+background-position: right 5px top 50%;
+outline: 0;
+box-shadow: none;
 `;
