5904a564ce87b77d7fa074a34d208d3253ba2de3
Idan <idankauf@gmail.com>
2022-07-10 16:54:39 +0300

    form handle submit been added

--- src/Comps/search/Search.tsx
+++ src/Comps/search/Search.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState ,useEffect} from 'react';
+import React, { ChangeEvent, useState ,useEffect,useRef} from 'react';
 import { SearchIcon } from "../../Icons";
 import useDebounce from '../../Hooks/useDebounce'
 import {
@@ -9,26 +9,34 @@ import {
 } from "./style";
 
 // import DropDown from '../dropdown/DropDown';
+interface SearchProps{
+  placeholder:string;
 
+}
 
 const Search = () => {
-  const [input, setInput] = useState<string>("");
-  const debouncedValue = useDebounce<string>(input, 500)
+  const [input, setInput] = useState<string | undefined>("");
+  const debouncedValue = useDebounce<string | undefined>(input, 500)
+  const textInput = useRef<HTMLInputElement>(null);
 
+  const handleSubmit=(event:React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    setInput(textInput?.current?.value)
+    console.log(input)
+  };
   const handleChange=(event: ChangeEvent<HTMLInputElement>)=>{
     setInput(event.currentTarget.value)
     console.log(input)
   };
-
   useEffect(() => {
    console.log(input)
   }, [debouncedValue])
   return (
-    <SearchFormContainer>
+    <SearchFormContainer onSubmit={handleSubmit}>
       <SearchButton>
         <SearchIcon />
       </SearchButton>
-      <Input onChange={handleChange}></Input>
+      <Input onChange={handleChange} ref={textInput}></Input>
       <VerticalLine></VerticalLine>
       {/* <DropDown/> */}
     </SearchFormContainer>
