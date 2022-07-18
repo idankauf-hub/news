9696a29aa184ac86f38f919948772a2c7cefe95b
Idan <idankauf@gmail.com>
2022-07-18 14:49:17 +0300

    first building

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import  {Navbar} from './Comps/navbar/Navbar';
-import Graph from './Comps/datesGraph/Graph';
-import { SearchMainPage } from './Screens/MainPage/components/SearchMainPage';
-import Underline from './Comps/underline/Underline';
-import Card from './Comps/card/Card';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar } from "./Comps/navbar/Navbar";
+import Graph from "./Comps/datesGraph/Graph";
+import { SearchMainPage } from "./Screens/MainPage/components/SearchMainPage";
+import Underline from "./Comps/underline/Underline";
+import Card from "./Comps/card/Card";
+import MainPage from "./Screens/MainPage/MainPage";
 function App() {
-  const data = [
-    {
-      month: "MAR",
-      frequency: 4000,
-    },
-    {
-      month: "APR",
-      frequency: 2000,
+  // const data = [
+  //   {
+  //     month: "MAR",
+  //     frequency: 4000,
+  //   },
+  //   {
+  //     month: "APR",
+  //     frequency: 2000,
 
-    },
-    {
-      month: "MAY",
-      frequency: 3000,
-    },
-    {
-      month: "JUN",
-      frequency: 2780,
-    },
-    {
-      month: "JUL",
-      frequency: 1890,
-    },
-    {
-      month: "AUG",
-      frequency: 2390,
-    },
-    {
-      month: "SEP",
-      frequency: 3490,
-    },
-  ];
-  return (<>
-    <Navbar/>
-    <Underline/>
+  //   },
+  //   {
+  //     month: "MAY",
+  //     frequency: 3000,
+  //   },
+  //   {
+  //     month: "JUN",
+  //     frequency: 2780,
+  //   },
+  //   {
+  //     month: "JUL",
+  //     frequency: 1890,
+  //   },
+  //   {
+  //     month: "AUG",
+  //     frequency: 2390,
+  //   },
+  //   {
+  //     month: "SEP",
+  //     frequency: 3490,
+  //   },
+  // ];
+  return (
+    <>
+      <MainPage />
     </>
   );
 }
--- src/Screens/MainPage/MainPage.tsx
+++ src/Screens/MainPage/MainPage.tsx
@@ -1,1 +1,17 @@
-export{}
+import React from 'react'
+import { Navbar } from '../../Comps/navbar/Navbar'
+import Underline from '../../Comps/underline/Underline'
+
+const MainPage = () => {
+  return (
+    <>
+    <Navbar/>
+    {/* <DropDowns/> */}
+    <Underline/>
+    {/* <Title/> */}
+    
+    </>
+  )
+}
+
+export default MainPage
