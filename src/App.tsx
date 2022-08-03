0c43f4714aff35aff7504b3bd283bedb2ddca39e
Idan <idankauf@gmail.com>
2022-08-03 14:00:38 +0300

    fixed merge

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 import "./App.css";
 import MainPage from "./Screens/MainPage/components/MainPage";
-
+import {Provider} from 'react-redux'
+import {store} from './store/store'
 function App() {
   return (
     <>
+      <Provider store={store}>
       <MainPage />
+      </Provider>
+
     </>
   );
 }
