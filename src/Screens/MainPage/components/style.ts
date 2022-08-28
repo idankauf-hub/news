f301ceda5c143929c56fe2acca3e78af14064491
Idan <idankauf@gmail.com>
2022-08-28 10:25:41 +0300

    styling

--- src/Screens/MainPage/components/style.ts
+++ src/Screens/MainPage/components/style.ts
@@ -8,11 +8,14 @@ export const Container = styled.div<Props>`
   display: flex;
   flex-direction: row;
   width: ${(props) => props.width};
+  @media (max-width: 600px) {
+    display: none;
+  }
   @media (max-width: 1450px) {
     margin-left: 4vw;
   }
   @media (max-width: 600px) {
-   width:70vw;
+    width: 70vw;
   }
   margin-left: 9vw;
   height: 50px;
